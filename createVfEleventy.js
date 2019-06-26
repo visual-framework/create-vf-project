46109ee3eb08b6f898eda1a5c01fb6846cd3ebe7
Ken Hawkins <khawkins98@gmail.com>
2019-06-26 15:46:44 +0200

    Use vf-eleventy alpha.2

--- createVfEleventy.js
+++ createVfEleventy.js
@@ -51,16 +51,16 @@ const createApp = () => {
       try {
         const spinner = new ora({
           prefixText: '🌍 ',
-        	text: 'Fetching github.com/visual-framework/vf-eleventy/archive/v2.0.0-alpha.1.zip',
+        	text: 'Fetching github.com/visual-framework/vf-eleventy/archive/v2.0.0-alpha.2.zip',
           // indent: 2,
         	// spinner: 'pong'
         });
 
 
-        download('https://github.com/visual-framework/vf-eleventy/archive/v2.0.0-alpha.1.zip').then(data => {
+        download('https://github.com/visual-framework/vf-eleventy/archive/v2.0.0-alpha.2.zip').then(data => {
           fs.writeFileSync('vf-eleventy.zip', data);
           resolve(true)
-          spinner.text = 'Fetched github.com/visual-framework/vf-eleventy/archive/v2.0.0-alpha.1.zip';
+          spinner.text = 'Fetched github.com/visual-framework/vf-eleventy/archive/v2.0.0-alpha.2.zip';
           spinner.succeed();
         });
 
@@ -106,7 +106,7 @@ const moveFiles = () => {
       // console.log(deleted);
     });
 
-    mv('temp/vf-eleventy-2.0.0-alpha.1', appName, {mkdirp: true}, function(err) {
+    mv('temp/vf-eleventy-2.0.0-alpha.2', appName, {mkdirp: true}, function(err) {
       console.log(`🚚  Files unpacked and moved into ./${appName}`)
       resolve()
     });
