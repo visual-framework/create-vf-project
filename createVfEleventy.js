114fb93a14157b4431456e35f4ba8239d1d4650c
Ken Hawkins <khawkins98@gmail.com>
2019-08-06 13:42:31 +0200

    Support vf-demo-design-system (#4)
    * Initial test
    
    * Map in bootstrap script
    
    * Bump version
    
    * Cosmetic tweaks
    
    * Update README.md
    
    * Guidance when done
    
    * JS problems
    
    * Command line tweaks
    
    * Use yarn instead
    
    * UI tweaks
    
    * Don't use git for cloning
    
    * Update package.json
    
    * Docs
    
    * Update createVfEleventy.js
    
    * add symlink-dir
    
    * Use vf-eleventy alpha.2
    
    * Update package.json
    
    * Fix order of operations
    
    * Tweaks
    
    * Use vf-eleventy alpha.2
    
    * Update with latest links to direct vf-core dependency
    
    * Pull in vf-eleventy alpha.5
    
    * Use alpha.6 of vf-eleventy
    
    * Udpate author
    
    * Capture second parameter, generif-y extraction
    
    * Programatically get zip folder information
    
    * Linting
    
    * Lookup URL to be used
    
    * Update version

--- createVfEleventy.js
+++ createVfEleventy.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const mv = require('mv');
 const del = require('delete');
-const symlinkDir = require('symlink-dir')
+const symlinkDir = require('symlink-dir');
 const exec = require('child_process').exec;
 const download = require('download');
 const unzipper = require('unzipper');
@@ -13,37 +13,62 @@ const unzipper = require('unzipper');
 // Make the console output a bit prettier
 const ora = require('ora');
 const cliSpinners = require('cli-spinners');
-const { red, blue, bold, underline } = require("colorette")
+const { red, blue, bold, underline } = require("colorette");
 
 const appName = process.argv.slice(2)[0];
-const appKind = process.argv.slice(2)[1] !== "--app";
-let appDirectory = `${process.cwd()}/${appName}`
+const appKind = process.argv.slice(2)[1];
+let appDirectory = `${process.cwd()}/${appName}`;
 
-// for now we only support vf-eleventy
-let kind = appKind ? "vf-eleventy" : "vf-eleventy";
+// Some reusable url, filenames
+let locations = {
+  zipName: 'source.zip',
+  tempLocation: 'temp',
+  zipFolderStem: 'notYetSet',
+  vfEleventy: 'https://github.com/visual-framework/vf-eleventy/archive/v2.0.0-alpha.6.zip',
+  vfDemoDesignSystem: 'https://github.com/visual-framework/vf-demo-design-system/archive/develop.zip'
+};
+
+// default to vf-eleventy
+let kind = appKind || "vf-eleventy";
+
+// track where we the user wants to get the package from
+let activeProjectUrl = 'notYetSet';
+switch (kind) {
+  case 'vf-eleventy':
+    activeProjectUrl = locations.vfEleventy;
+    break;
+  case 'vf-demo-design-system':
+    activeProjectUrl = locations.vfDemoDesignSystem;
+    break;
+  default:
+    // default to vf-eleventy
+    console.log(`\n⚠️  We didn\'t see or understand the project template request, we'll proceed with using the vf-eleventy boilerplate.\n`);
+    activeProjectUrl = locations.vfEleventy;
+    break;
+}
 
 const run = async () => {
-  console.log(` --------------\n`)
-  console.log(bold(`\n🚧  Getting started!\n`))
+  console.log(` --------------\n`);
+  console.log(bold(`\n🚧  Getting started!\n`));
 
-  let success = await createApp()
+  let success = await createApp();
   if (!success) {
-    console.log(`\n😭  Something went wrong when initing ${kind}`)
-    console.log(`\n⚠️  Tip: Make sure the directory "${appName}" does not already exsist.\n`)
+    console.log(`\n😭  Something went wrong when initing ${kind}`);
+    console.log(`\n⚠️  Tip: Make sure the directory "${appName}" does not already exsist.\n`);
     return false;
   } else {
-    await unzipArchive()
-    await moveFiles()
-    await cdIntoNewApp()
-    await installPackages()
-    await restoreSymLink()
-    console.log(bold("🎉  All done!\n"))
-    console.log(`⌨️   You're now ready to develop:`)
-    console.log(`      1. cd ${appName}`)
-    console.log(`      2. gulp dev\n`)
-    console.log(` --------------\n`)
+    await unzipArchive();
+    await moveFiles();
+    await cdIntoNewApp();
+    await installPackages();
+    await restoreSymLink();
+    console.log(bold("🎉  All done!\n"));
+    console.log(`⌨️   You're now ready to develop:`);
+    console.log(`      1. cd ${appName}`);
+    console.log(`      2. gulp dev\n`);
+    console.log(` --------------\n`);
   }
-}
+};
 
 const createApp = () => {
   return new Promise((resolve) => {
@@ -51,73 +76,77 @@ const createApp = () => {
       try {
         const spinner = new ora({
           prefixText: '🌍 ',
-        	text: 'Fetching github.com/visual-framework/vf-eleventy/archive/v2.0.0-alpha.4.zip',
+        	text: 'Fetching ' + activeProjectUrl,
           // indent: 2,
         	// spinner: 'pong'
         });
 
-
-        download('https://github.com/visual-framework/vf-eleventy/archive/v2.0.0-alpha.4.zip').then(data => {
-          fs.writeFileSync('vf-eleventy.zip', data);
-          resolve(true)
-          spinner.text = 'Fetched github.com/visual-framework/vf-eleventy/archive/v2.0.0-alpha.4.zip';
+        download(activeProjectUrl).then(data => {
+          fs.writeFileSync(locations.zipName, data);
+          resolve(true);
+          spinner.text = 'Fetched ' + activeProjectUrl;
           spinner.succeed();
         });
 
-        //
-        // exec(`git clone --depth 1 -b master https://github.com/visual-framework/${kind} "${appName}"`, (error, stdout, stderr) => {
-        //   if (error) {
-        //     resolve(false)
-        //   } else {
-        //     console.log(`\n✨  Cloned ${kind} into "${appName}"`)
-        //     resolve(true)
-        //   }
-        // })
       } catch(e) {
-        console.error(`\n⚠️  Couldn't get archive of ${kind}"`)
-        resolve(false)
+        console.error(`\n⚠️  Couldn't get archive of ${kind}"`);
+        resolve(false);
       }
     } else {
-      console.log("\nNo app name was provided.")
-      console.log("\nProvide an app name in the following format: ")
-      console.log("\nnpm init @visual-framework/vf-eleventy", `"app-name"\n`)
-        resolve(false)
+      console.log("\nNo app name was provided.");
+      console.log("\nProvide an app name in the following format: ");
+      console.log("\nnpm init @visual-framework/vf-eleventy", `"app-name"\n`);
+      resolve(false);
     }
-  })
-}
+  });
+};
 
 const unzipArchive = () => {
   return new Promise((resolve) => {
-    fs.createReadStream('vf-eleventy.zip')
-      .pipe(unzipper.Extract({ path: 'temp' }))
-      .on('entry', entry => entry.autodrain())
+
+    // tap into the zip to see the parent folder name
+    fs.createReadStream(locations.zipName)
+      .pipe(unzipper.ParseOne('README.md'))
+      .on('entry', function (entry) {
+        locations.zipFolderStem = entry.path.split('/')[0];
+        entry.autodrain();
+      });
+
+    // extract contents to temp location
+    fs.createReadStream(locations.zipName)
+      .pipe(unzipper.Extract({ path: locations.tempLocation }))
+      // .pipe(unzipper.Parse())
+      // .on('entry', entry => entry.autodrain())
+      .on('entry', function (entry) {
+        const fileName = entry.path;
+        const type = entry.type; // 'Directory' or 'File'
+        const size = entry.vars.uncompressedSize; // There is also compressedSize;
+        entry.autodrain();
+      })
       .promise()
       .then( () => resolve(), e => console.log('⚠️  Error unzipping',e));
-  })
-}
-
+  });
+};
 
 const moveFiles = () => {
   return new Promise((resolve) => {
-    del(['vf-eleventy.zip'], function(err, deleted) {
+    del([locations.zipName], function(err, deleted) {
       if (err) throw err;
-      // deleted files
-      // console.log(deleted);
     });
-    mv('temp/vf-eleventy-2.0.0-alpha.4', appName, {mkdirp: true}, function(err) {
-      console.log(`🚚  Files unpacked and moved into ./${appName}`)
-      resolve()
+    mv(locations.tempLocation+'/'+locations.zipFolderStem, appName, {mkdirp: true}, function(err) {
+      console.log(`🚚  Files unpacked and moved into ./${appName}`);
+      resolve();
     });
-  })
-}
+  });
+};
 
 const cdIntoNewApp = () => {
   return new Promise((resolve) => {
-    console.log(`🗺   Switching to the ./${appName} directory`)
+    console.log(`🗺   Switching to the ./${appName} directory`);
     process.chdir(`${appName}`);
-    resolve()
-  })
-}
+    resolve();
+  });
+};
 
 const installPackages = () => {
   return new Promise((resolve) => {
@@ -125,8 +154,6 @@ const installPackages = () => {
     // remove the defunct symlink
     del(['src/components/vf-core-components'], function(err, deleted) {
       if (err) throw err;
-      // deleted files
-      // console.log(deleted);
     });
 
     const spinner = new ora({
@@ -146,10 +173,28 @@ const installPackages = () => {
     spinner.start();
     exec(`yarn install`, () => {
       spinner.succeed();
-      resolve()
-    })
-  })
-}
+      resolve();
+    });
+  });
+};
+
+// restore a symlink from src/vf-components/vf-core-components to node_modules/@visual-framework/vf-core/components
+const restoreSymLink = () => {
+  return new Promise((resolve) => {
+    symlinkDir('node_modules/\@visual-framework', 'src/components/vf-core-components')
+      .then(result => {
+        // console.log(result)
+
+        resolve();
+
+        return symlinkDir('node_modules/\@visual-framework', 'src/components/vf-core-components');
+
+      })
+      .catch(err => console.error(err));
+    // console.log(`🗺  Switching to the ./${appName} directory`)
+    // process.chdir(`${appName}`);
+  });
+};
 
 // restore a symlink from src/vf-components/vf-core-components to node_modules/@visual-framework/vf-core/components
 const restoreSymLink = () => {
